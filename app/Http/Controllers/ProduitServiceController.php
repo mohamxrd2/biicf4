61330ade100ff517f41878d7ed5781438ac7fbec
mohamxrd2 <pro.mohamxrd@gmail.com>
2024-06-07 17:43:49 +0000

    add

--- app/Http/Controllers/ProduitServiceController.php
+++ app/Http/Controllers/ProduitServiceController.php
@@ -223,9 +223,9 @@ class ProduitServiceController extends Controller
 
             // Récupérer la date la plus ancienne parmi les achats groupés pour ce produit
             $datePlusAncienne = AchatGrouper::where('idProd', $produit->id)->min('created_at');
-            //$tempEcoule = Carbon::now()->addDays(5);
+            $tempEcoule = Carbon::now()->addDays(5);
             // Vérifier si la date la plus ancienne + 5 jours est dépassée
-            $tempEcoule = Carbon::now()->subDays(1); // pour le test
+            //$tempEcoule = Carbon::now()->subDays(1); // pour le test
 
             // Initialiser les variables pour la vue
             $sommeQuantite = AchatGrouper::where('idProd', $produit->id)->sum('quantité');
