59c0f7c797e1f456a6ae73a57054d178fce9d09f
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-07-05 18:43:52 +0000

    chance5

--- resources/views/admin/layout/navside.blade.php
+++ resources/views/admin/layout/navside.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <meta name="user-id" content="{{ auth()->user()->id }}"> --}}
+    <meta name="user-id" content="{{ auth()->user()->id }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
@@ -585,24 +585,6 @@
 
 
     @livewireScripts
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <script>
-        Livewire.on('swal:toast', param => {
-            Swal.fire({
-                toast: true,
-                position: 'top-end',
-                icon: 'success', // Icône passée en paramètre depuis Livewire
-                title: 'action effectué avec succes',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true
-            });
-        });
-    </script>
-
-
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 
 </body>
