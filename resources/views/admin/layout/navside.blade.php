3c0cdfe11ea06e5af68a5aacc80afd0fa0f2ebea
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-07-05 18:46:03 +0000

    chance5

--- resources/views/admin/layout/navside.blade.php
+++ resources/views/admin/layout/navside.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="user-id" content="{{ auth()->user()->id }}">
+    {{-- <meta name="user-id" content="{{ auth()->user()->id }}"> --}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
