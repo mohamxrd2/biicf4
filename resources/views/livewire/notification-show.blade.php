410ae025c23089324697354a7ec16c6b8af67202
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-07-11 07:43:06 +0000

    chance

--- resources/views/livewire/notification-show.blade.php
+++ resources/views/livewire/notification-show.blade.php
@@ -812,7 +812,8 @@
             <header class="mb-4">
                 <h1 class="text-3xl font-bold">Facture Proformat</h1>
                 <div class="text-gray-600">
-                    <p>code la de Facture: <span class="font-semibold">#{{$notification->data['code_unique']}}</span></p>
+                    <p>code la de Facture: <span
+                            class="font-semibold">#{{ $notification->data['code_unique'] }}</span></p>
                     <p>Date: <span class="font-semibold">09 juillet 2024</span></p>
                 </div>
             </header>
@@ -843,13 +844,14 @@
                             <td class="py-2 px-4 border-b">Produit commandé: {{ $produitfat->name }}</td>
                             <td class="py-2 px-4 border-b">{{ $notification->data['quantiteC'] }}</td>
                             <td class="py-2 px-4 border-b">{{ $produitfat->prix }} FCFA</td>
-                            <td class="py-2 px-4 border-b"> {{ (int) ($notification->data['prixTrade'] * $produitfat->prix) }} FCFA</td>
+                            <td class="py-2 px-4 border-b">
+                                {{ (int) ($notification->data['prixTrade'] * $produitfat->prix) }} FCFA</td>
                         </tr>
                         <tr>
                             <td class="py-2 px-4 border-b">Livraiveur: {{ $userFour->name }}</td>
                             <td class="py-2 px-4 border-b">N/A</td>
-                            <td class="py-2 px-4 border-b">{{$notification->data['prixTrade']}} FCFA</td>
-                            <td class="py-2 px-4 border-b">{{$notification->data['prixTrade']}} FCFA</td>
+                            <td class="py-2 px-4 border-b">{{ $notification->data['prixTrade'] }} FCFA</td>
+                            <td class="py-2 px-4 border-b">{{ $notification->data['prixTrade'] }} FCFA</td>
                         </tr>
                     </tbody>
                 </table>
@@ -858,12 +860,28 @@
             <section class="mb-6 flex justify-between">
                 <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
                     <div class="flex space-x-2 mt-4">
-                        <button wire:click.prevent='valider' class="bg-green-800 text-white px-4 py-2 rounded-lg">Validez la commande</button>
+                        <button wire:click.prevent='valider' class="bg-green-800 text-white px-4 py-2 rounded-lg relative">
+                            <!-- Texte du bouton et icône -->
+                            <span wire:loading.remove>
+                                Validez la commande
+                            </span>
+                            <span wire:loading>
+                                Chargement...
+                                <svg class="w-5 h-5 animate-spin inline-block ml-2" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M12 4.354a7.646 7.646 0 100 15.292 7.646 7.646 0 000-15.292zm0 0V1m0 3.354a7.646 7.646 0 100 15.292 7.646 7.646 0 000-15.292z" />
+                                </svg>
+                            </span>
+                        </button>
+
                     </div>
                 </div>
 
                 <div class="w-1/4 bg-gray-100 p-2 rounded-lg">
-                    <p class="text-2xl text-center font-bold">Total TTC: <span class="font-bold">{{ (int) ($notification->data['prixTrade'] * $produitfat->prix) +$notification->data['prixTrade'] }} FCFA</span></p>
+                    <p class="text-2xl text-center font-bold">Total TTC: <span
+                            class="font-bold">{{ (int) ($notification->data['prixTrade'] * $produitfat->prix) + $notification->data['prixTrade'] }}
+                            FCFA</span></p>
                 </div>
             </section>
 
@@ -871,16 +889,8 @@
                 <p class="text-gray-600 text-center">Merci pour votre confiance.</p>
             </footer>
         </div>
-        {{-- <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                window.addEventListener('alert-start', event => {
-                    alert(event.detail.message);
-                });
-            });
-        </script> --}}
-
     @elseif ($notification->type === 'App\Notifications\livraisonVerif')
-        <div  class="grid grid-cols-2 gap-4 p-4">
+        <div class="grid grid-cols-2 gap-4 p-4">
             <div class="lg:col-span-1 col-span-2">
 
                 <h2 class="text-3xl font-semibold mb-2">{{ $produitfat->name }}</h2>
@@ -916,7 +926,7 @@
                 </div>
 
             </div>
-            <div  class="lg:col-span-1 col-span-2">
+            <div class="lg:col-span-1 col-span-2">
 
                 <div class="p-4">
 
