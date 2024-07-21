ecc066b15be49960d1a59fee3dc3130569b1d674
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-07-21 18:12:03 +0000

    poit de depart

--- app/Console/Commands/GrouperFacture.php
+++ app/Console/Commands/GrouperFacture.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Comment;
-use App\Models\Countdown;
+use App\Models\groupagefact;
 use App\Models\User;
 use App\Notifications\GrouperFactureNotifications;
 use Illuminate\Console\Command;
@@ -11,85 +11,70 @@ use Illuminate\Support\Facades\Notification;
 
 class GrouperFacture extends Command
 {
-
     protected $signature = 'app:grouper-facture';
-
-    protected $description = 'Command description';
+    protected $description = 'Command to send notifications for grouped invoices';
 
     public function handle()
     {
-        // $appelOffreGroups = AppelOffreGrouper::whereNotNull('notified')
-        //     ->where('created_at', '<=', now()->subMinutes(1))
-        //     ->get();
+        $groupagefacts = groupagefact::where('notified', false)
+            ->where('start_time', '<=', now()->subMinutes(1))
+            ->get();
 
-        // foreach ($appelOffreGroups as $appelOffreGroup) {
+        foreach ($groupagefacts as $groupagefact) {
             // Récupérer le code unique
-    //         $code_unique = $countdown->code_unique;
+            $code_unique = $groupagefact->code_unique;
 
-    //         // Retrouver l'enregistrement avec le prix le plus bas parmi les enregistrements avec ce code unique
-    //         $lowestPriceComment = Comment::with('user')
-    //             ->where('code_unique', $code_unique)
-    //             ->orderBy('prixTrade', 'asc')
-    //             ->first();
+            // Retrouver l'enregistrement avec le prix le plus bas parmi les enregistrements avec ce code unique
+            $lowestPriceComment = Comment::with('user')
+                ->where('code_unique', $code_unique)
+                ->orderBy('prixTrade', 'asc')
+                ->first();
 
+            if ($lowestPriceComment) {
+                $lowestPrice = $lowestPriceComment->prixTrade;
+                $traderId = $lowestPriceComment->id_trader;
+                $id_prod = $lowestPriceComment->id_prod;
+                $quantiteC = $lowestPriceComment->quantiteC;
 
-    //         // Vérifier si un enregistrement a été trouvé
-    //         if ($lowestPriceComment) {
-    //             $lowestPrice = $lowestPriceComment->prixTrade;
-    //             $traderId = $lowestPriceComment->id_trader;
-    //             $id_prod = $lowestPriceComment->id_prod;
-    //             $quantiteC = $lowestPriceComment->quantiteC;
-    //             $localite = $lowestPriceComment->localite;
-    //             $specificite = $lowestPriceComment->specificite;
-    //             $nameprod = $lowestPriceComment->nameprod;
-    //             $id_sender = $lowestPriceComment->id_sender;
+                $usersenders = groupagefact::where('code_unique', $code_unique)
+                    ->distinct()
+                    ->pluck('usersenders')
+                    ->toArray();
 
-    //         $usergroup = AppelOffreGrouper::where('codeunique', $codesUniques)
-    //             ->distinct()
-    //             ->pluck('user_id')
-    //             ->toArray();
+                $decodedProdUsers = [];
+                foreach ($usersenders as $usersender) {
+                    $decodedValues = json_decode($usersender, true);
+                    if (is_array($decodedValues)) {
+                        $decodedProdUsers = array_merge($decodedProdUsers, $decodedValues);
+                    }
+                }
 
-    //         $prodUsers = AppelOffreGrouper::where('codeunique', $codesUniques)
-    //             ->distinct()
-    //             ->pluck('prodUsers')
-    //             ->toArray();
+                foreach ($decodedProdUsers as $prodUser) {
+                    $data = [
+                        'code_unique' => $code_unique,
+                        'prixTrade' => $lowestPrice,
+                        'id_trader' => $traderId,
+                        'idProd' => $id_prod,
+                        'quantiteC' => $quantiteC,
+                    ];
 
-    //         $decodedProdUsers = [];
-    //         foreach ($prodUsers as $prodUser) {
-    //             $decodedValues = json_decode($prodUser, true);
-    //             if (is_array($decodedValues)) {
-    //                 $decodedProdUsers = array_merge($decodedProdUsers, $decodedValues);
-    //             }
-    //         }
+                    $requiredKeys = ['code_unique', 'prixTrade', 'id_trader', 'idProd', 'quantiteC'];
+                    foreach ($requiredKeys as $key) {
+                        if (!array_key_exists($key, $data)) {
+                            throw new \InvalidArgumentException("La clé '$key' est manquante dans \$data.");
+                        }
+                    }
 
+                    $owner = User::find($prodUser);
 
+                    if ($owner) {
+                        Notification::send($owner, new GrouperFactureNotifications($data));
+                    }
+                }
 
-    //         foreach ($decodedProdUsers as $prodUser) {
-    //             $data = [
-    //                 'sender_name' => $countdown->sender->name, // Ajouter le nom de l'expéditeur aux détails de la notification
-    //                 'code_unique' => $countdown->code_unique,
-    //                 'prixTrade' => $lowestPrice,
-    //                 'id_trader' => $traderId,
-    //                 'idProd' => $id_prod,
-    //                 'quantiteC' => $quantiteC,
-    //             ];
-
-    //             $requiredKeys = ['dateTot', 'dateTard', 'productName', 'quantity', 'payment', 'Livraison', 'specificity', 'image', 'prodUsers', 'code_unique'];
-    //             foreach ($requiredKeys as $key) {
-    //                 if (!array_key_exists($key, $data)) {
-    //                     throw new \InvalidArgumentException("La clé '$key' est manquante dans \$data.");
-    //                 }
-    //             }
-
-    //             $owner = User::find($prodUser);
-
-    //             if ($owner) {
-    //                 Notification::send($owner, new GrouperFactureNotifications($data));
-    //             }
-    //         }
-
-    //     }
-
-    //     return 0;
+                // Mettre à jour la facture pour indiquer qu'une notification a été envoyée
+                $groupagefact->update(['notified' => true]);
+            }
+        }
     }
 }
--- resources/views/livewire/notif.blade.php
+++ resources/views/livewire/notif.blade.php
@@ -453,6 +453,42 @@
 
                         </div>
                     </a>
+                @elseif ($notification->type === 'App\Notifications\GrouperFactureNotifications')
+                    <a href="{{ route('notification.show', $notification->id) }}">
+
+                        <div class="flex w-full">
+                            <div class=" w-16 h-16  overflow-hidden mr-3">
+
+                                <svg class="w-full text-slate-400" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m6.621 9.879a3 3 0 0 0-5.02 2.897l.164.609a4.5 4.5 0 0 1-.108 2.676l-.157.439.44-.22a2.863 2.863 0 0 1 2.185-.155c.72.24 1.507.184 2.186-.155L15 18M8.25 15.75H12m-1.5-13.5H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                </svg>
+
+
+                            </div>
+
+                            <div class="flex flex-col justify-between w-full">
+                                <div class="flex justify-between items-center w-full">
+                                    <h3 class="text-md font-semibold">Facture proformat</h3>
+                                    <p class="text-[12px] text-gray-400 text-right">
+                                        {{ \Carbon\Carbon::parse($notification->created_at)->diffForHumans() }}
+                                    </p>
+                                </div>
+                                <div class="flex justify-between items-center w-full h-full">
+                                    <p class="text-sm text-slate-500 l max-w-1/2 font-normal">Vous avez été
+                                        identifié discustion entre les livreurs terminées consulter votre facture
+                                        proformat</p>
+                                    @if ($notification->read_at == null)
+                                        <div class="w-10 flex justify-center items-center">
+                                            <span class="w-2 h-2 rounded-full bg-purple-700"></span>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+
+                        </div>
+                    </a>
                 @elseif ($notification->type === 'App\Notifications\livraisonVerif')
                     <a href="{{ route('notification.show', $notification->id) }}">
                         @php
