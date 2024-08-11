f43248164b7894ff8cce263b14df4ab6ecea09c9
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-08-11 20:59:44 +0000

    correction livraison noffre

--- app/Console/Commands/CheckCountdowns.php
+++ app/Console/Commands/CheckCountdowns.php
@@ -22,7 +22,6 @@ class CheckCountdowns extends Command
 
     public function handle()
     {
-        // Récupérer les countdowns non notifiés et dont le start_time est passé depuis au moins une minute
         $countdowns = Countdown::where('notified', false)
             ->where('start_time', '<=', now()->subMinutes(1))
             ->with('sender') // Charger la relation userSender
@@ -38,78 +37,90 @@ class CheckCountdowns extends Command
                 ->orderBy('prixTrade', 'asc')
                 ->first();
 
+            // Retrouver l'enregistrement avec le prix le plus élevé parmi les enregistrements avec ce code unique
+            $highestPriceComment = Comment::with('user')
+                ->where('code_unique', $code_unique)
+                ->orderBy('prixTrade', 'desc')
+                ->first();
 
             // Vérifier si un enregistrement a été trouvé
             if ($lowestPriceComment) {
-                $lowestPrice = $lowestPriceComment->prixTrade;
-                $traderId = $lowestPriceComment->id_trader;
-                $id_prod = $lowestPriceComment->id_prod;
-                $quantiteC = $lowestPriceComment->quantiteC;
-                $localite = $lowestPriceComment->localite;
-                $specificite = $lowestPriceComment->specificite;
-                $nameprod = $lowestPriceComment->nameprod;
-                $id_sender = $lowestPriceComment->id_sender;
-                $prixProd = $lowestPriceComment->prixProd;
+                // Déterminer quel commentaire utiliser en fonction de la différence
+                $commentToUse = ($countdown->difference === 'offredirect') ? $highestPriceComment : $lowestPriceComment;
 
-                // Décoder le JSON id_sender
-                $decodedSenderIds = json_decode($id_sender, true);
+                if ($commentToUse) {
+                    $price = $commentToUse->prixTrade;
+                    $traderId = $commentToUse->id_trader;
+                    $id_prod = $commentToUse->id_prod;
+                    $quantiteC = $commentToUse->quantiteC;
+                    $localite = $commentToUse->localite;
+                    $specificite = $commentToUse->specificite;
+                    $nameprod = $commentToUse->nameprod;
+                    $id_sender = $commentToUse->id_sender;
+                    $prixProd = $commentToUse->prixProd;
 
-                // Calculer montotal
-                $montotal = $quantiteC * $lowestPrice;
+                    // Décoder le JSON id_sender
+                    $decodedSenderIds = json_decode($id_sender, true);
 
-                // Définir les détails de la notification
-                $details = [
-                    'sender_name' => $countdown->sender->id ?? null, // Ajouter le nom de l'expéditeur aux détails de la notification
-                    'code_unique' => $countdown->code_unique,
-                    'prixTrade' => $lowestPrice,
-                    'id_trader' => $traderId,
-                    'idProd' => $id_prod,
-                    'quantiteC' => $quantiteC,
-                    'prixProd' => $prixProd,
-                ];
-                $Adetails = [
-                    'code_unique' => $countdown->code_unique,
-                    'prixTrade' => $lowestPrice,
-                    'id_trader' => $traderId,
-                    'quantiteC' => $quantiteC,
-                    'localite' => $localite,
-                    'specificite' => $specificite,
-                    'nameprod' => $nameprod,
-                    'id_sender' => $decodedSenderIds,
-                    'montantTotal' => $montotal,
-                ];
+                    // Calculer montotal
+                    $montotal = $quantiteC * $price;
 
-                $Gdetails = [
-                    'code_unique' => $countdown->code_unique,
-                    'prixTrade' => $lowestPrice,
-                    'id_trader' => $traderId,
-                    'quantiteC' => $quantiteC,
-                    'localite' => $localite,
-                    'specificite' => $specificite,
-                    'nameprod' => $nameprod,
-                    'id_sender' => $decodedSenderIds,
-                    'montantTotal' => $montotal,
-                ];
+                    // Définir les détails de la notification
+                    $details = [
+                        'sender_name' => $countdown->sender->id ?? null, // Ajouter le nom de l'expéditeur aux détails de la notification
+                        'code_unique' => $countdown->code_unique,
+                        'prixTrade' => $price,
+                        'id_trader' => $traderId,
+                        'idProd' => $id_prod,
+                        'quantiteC' => $quantiteC,
+                        'prixProd' => $prixProd,
+                    ];
 
-                // Vérifier si la colonne 'difference' est égale à 'single'
-                if ($countdown->difference === 'single') {
-                    // Envoyer la notification à l'utilisateur expéditeur
-                    Notification::send($lowestPriceComment->user, new AppelOffreTerminer($Adetails));
-                } else if ($countdown->difference === 'offredirect') {
-                    // Envoyer la notification à l'utilisateur expéditeur
-                    Notification::send($lowestPriceComment->user, new NegosTerminer($details));
-                } else if ($countdown->difference === 'grouper') {
-                    // Envoyer la notification à l'utilisateur expéditeur
-                    Notification::send($lowestPriceComment->user, new AppelOffreTerminer($Gdetails));
-                } else {
-                    // Envoyer une autre notification ou effectuer une autre action
-                    Notification::send($countdown->sender, new CountdownNotification($details));
+                    $Adetails = [
+                        'code_unique' => $countdown->code_unique,
+                        'prixTrade' => $price,
+                        'id_trader' => $traderId,
+                        'quantiteC' => $quantiteC,
+                        'localite' => $localite,
+                        'specificite' => $specificite,
+                        'nameprod' => $nameprod,
+                        'id_sender' => $decodedSenderIds,
+                        'montantTotal' => $montotal,
+                    ];
+
+                    $Gdetails = [
+                        'code_unique' => $countdown->code_unique,
+                        'prixTrade' => $price,
+                        'id_trader' => $traderId,
+                        'quantiteC' => $quantiteC,
+                        'localite' => $localite,
+                        'specificite' => $specificite,
+                        'nameprod' => $nameprod,
+                        'id_sender' => $decodedSenderIds,
+                        'montantTotal' => $montotal,
+                    ];
+
+                    // Vérifier le type de notification à envoyer
+                    if ($countdown->difference === 'single') {
+                        // Envoyer la notification avec le prix le plus élevé
+                        Notification::send($commentToUse->user, new AppelOffreTerminer($Adetails));
+                    } else if ($countdown->difference === 'offredirect') {
+                        // Envoyer la notification avec le prix le plus bas
+                        Notification::send($lowestPriceComment->user, new NegosTerminer($details));
+                    } else if ($countdown->difference === 'grouper') {
+                        // Envoyer la notification avec le prix le plus bas
+                        Notification::send($lowestPriceComment->user, new AppelOffreTerminer($Gdetails));
+                    } else {
+                        // Envoyer une autre notification ou effectuer une autre action
+                        Notification::send($countdown->sender, new CountdownNotification($details));
+                    }
+
+                    // Supprimer les commentaires avec ce code unique
+                    Comment::where('code_unique', $code_unique)->delete();
+
+                    // Mettre à jour le statut notified à true
+                    $countdown->update(['notified' => true]);
                 }
-                // Supprimer les commentaires avec ce code unique
-                Comment::where('code_unique', $code_unique)->delete();
-
-                // Mettre à jour le statut notified à true
-                $countdown->update(['notified' => true]);
             }
         }
     }
