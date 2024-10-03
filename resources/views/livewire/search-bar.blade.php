f3b66ee691b05de49b5fcf12b61293f0f8638d31
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-10-03 17:01:19 +0000

    acceuil 2

--- resources/views/livewire/search-bar.blade.php
+++ resources/views/livewire/search-bar.blade.php
@@ -61,7 +61,15 @@
 
 
 
-    {{--
+    {{-- <a href="{{ route('biicf.postdet', $produit->id) }}">
+                <div
+                    class="w-full sm:w-48 md:w-56 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div class="h-36 w-full mr-2 ">
+                        <img class="w-full h-full rounded-xl  object-cover"
+                            src="{{ $produit->photoProd1 ? asset('post/all/' . $produit->photoProd1) : asset('img/noimg.jpeg') }}"
+                            alt="">
+                    </div>
                     <div class="p-2 flex-grow">
                         <div class="flex items-center justify-between">
 
@@ -99,54 +107,58 @@
                                 {{ \Carbon\Carbon::parse($produit->created_at)->diffForHumans() }}</p>
                         </div>
                     </div>
-            </a> --}}
-    <main class="p-4 md:ml-64 h-auto pt-20">
-        <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-            @foreach ($produits as $produit)
-                <div
-                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="{{ route('biicf.postdet', $produit->id) }}">
-                        <img class="rounded-t-lg"
-                            src="{{ $produit->photoProd1 ? asset('post/all/' . $produit->photoProd1) : asset('img/noimg.jpeg') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-2">
-                        <div class="flex items-center justify-between">
+                </div>
+        </a> --}}
+    <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+        @foreach ($produits as $produit)
+            <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg"
+                        src="{{ $produit->photoProd1 ? asset('post/all/' . $produit->photoProd1) : asset('img/noimg.jpeg') }}"
+                        alt="" />
+                </a>
+                <div class="p-2 flex-grow">
+                    <div class="flex items-center justify-between">
 
-                            <h5 class="text-xl sm:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                {{ $produit->name }}
-                            </h5>
-                            @if ($produit->type == 'Produit')
-                                <span
-                                    class="inline-flex px-2.5 py-0.5 rounded-full text-xs font-medium leading-none  text-green-800 bg-green-100">Produit</span>
-                            @else
-                                <span
-                                    class="inline-flex px-2.5 py-0.5 rounded-full text-xs font-medium leading-none  text-yellow-800 bg-yellow-100">Service</span>
-                            @endif
-                        </div>
-                        <h1 class="text-base sm:text-base font-bold tracking-tight text-gray-900 dark:text-white">
-                            {{ $produit->prix }} XOF
-                        </h1>
-                        <div class="flex items-center text-[12px] text-gray-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class="w-4 h-4 inline-block align-middle">
-                                <path fill-rule="evenodd"
-                                    d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-                                    clip-rule="evenodd" />
-                            </svg>
+                        <h5 class="text-xl sm:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            {{ $produit->name }}
+                        </h5>
+                        @if ($produit->type == 'Produit')
+                            <span
+                                class="inline-flex px-2.5 py-0.5 rounded-full text-xs font-medium leading-none  text-green-800 bg-green-100">Produit</span>
+                        @else
+                            <span
+                                class="inline-flex px-2.5 py-0.5 rounded-full text-xs font-medium leading-none  text-yellow-800 bg-yellow-100">Service</span>
+                        @endif
+                    </div>
+                    <h1 class="text-base sm:text-base font-bold tracking-tight text-gray-900 dark:text-white">
+                        {{ $produit->prix }} XOF
+                    </h1>
 
-                            <p class="font-normal text-gray-700 dark:text-gray-400">{{ $produit->comnServ }}</p>,
-                            <p class="font-normal text-gray-700 dark:text-gray-400">{{ $produit->villeServ }}</p>
-                        </div>
-                        <div class="w-full bottom-0 ml-1">
-                            <p class="text-sm text-gray-600 text-left">
-                                {{ \Carbon\Carbon::parse($produit->created_at)->diffForHumans() }}</p>
-                        </div>
+                    <hr>
+
+                    <div class="flex items-center text-[12px] text-gray-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="w-4 h-4 inline-block align-middle">
+                            <path fill-rule="evenodd"
+                                d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                                clip-rule="evenodd" />
+                        </svg>
+
+                        <p class="font-normal text-gray-700 dark:text-gray-400">{{ $produit->comnServ }}</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">{{ $produit->villeServ }}</p>
+                    </div>
+
+                    <div class="w-full bottom-0 ml-1">
+                        <p class="text-sm text-gray-600 text-left">
+                            {{ \Carbon\Carbon::parse($produit->created_at)->diffForHumans() }}</p>
                     </div>
                 </div>
-            @endforeach
-        </div>
-    </main>
+
+            </div>
+        @endforeach
+    </div>
 
     {{-- </div> --}}
 </div>
