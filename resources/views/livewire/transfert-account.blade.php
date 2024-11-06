5bf617204df40377d325a3107088684e39757b51
MacBook Pro <macbookpro@MacBook-Pro-de-Mohamed.local>
2024-11-06 08:49:30 +0000

    add

--- resources/views/livewire/transfert-account.blade.php
+++ resources/views/livewire/transfert-account.blade.php
@@ -1,24 +1,24 @@
 <div class="flex justify-center mt-4">
     {{-- In work, do what you enjoy. --}}
 
-    <form wire:submit.prevent="submitTransfert" class="w-full md:w-1/2 bg-white border border-gray-300 rounded-xl p-4">
+    <form wire:submit.prevent="submitTransfert" class="w-full p-4 bg-white border border-gray-300 md:w-1/2 rounded-xl">
         @if (session()->has('successMessage'))
-            <div class="bg-green-400 text-white p-4 rounded-md mt-2 mb-6">
+            <div class="p-4 mt-2 mb-6 text-white bg-green-400 rounded-md">
                 {{ session('successMessage') }}
             </div>
         @endif
 
         @if (session()->has('errorMessage'))
-            <div class="bg-red-400 text-white p-4 rounded-md mt-2 mb-6">
+            <div class="p-4 mt-2 mb-6 text-white bg-red-400 rounded-md">
                 {{ session('errorMessage') }}
             </div>
         @endif
-        <h2 class="text-center font-semibold text-xl">Transfert entre compte</h2>
+        <h2 class="text-xl font-semibold text-center">Transfert entre compte</h2>
 
         <div class="mt-4">
 
             <input type="number" id="amount" wire:model="amount" required
-                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 placeholder="Entrez le montant" required>
             @error('amount')
                 <span class="text-red-500">{{ $message }}</span>
@@ -26,12 +26,11 @@
         </div>
         <div class="mt-4">
             <select name="" id="" wire:model="account1" required
-                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                 <option value="" disabled>Choisissez le compte de prélevement</option>
                 <option value="COC">COC</option>
                 <option value="COI">COI</option>
                 <option value="CEDD">CEDD</option>
-                <option value="CFA">CFA</option>
                 <option value="CEFP">CEFP</option>
 
             </select>
@@ -44,12 +43,11 @@
 
         <div class="mt-4">
             <select name="" id="" wire:model="account2" required
-                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                 <option value="" disabled>Choisissez le compte de reception</option>
                 <option value="COC">COC</option>
                 <option value="COI">COI</option>
                 <option value="CEDD">CEDD</option>
-                <option value="CFA">CFA</option>
                 <option value="CEFP">CEFP</option>
 
             </select>
@@ -60,9 +58,9 @@
 
         </div>
 
-        <div class="mt-6 relative">
+        <div class="relative mt-6">
             <button type="submit"
-                class="w-full py-3 bg-purple-600 hover:bg-purple-700 transition-colors rounded-md text-white font-medium"
+                class="w-full py-3 font-medium text-white transition-colors bg-purple-600 rounded-md hover:bg-purple-700"
                 wire:loading.attr="disabled">
                 Soumettre
             </button>
@@ -70,7 +68,7 @@
             <!-- Texte de chargement -->
             <div wire:loading wire:target="submitDeposit"
                 class="absolute inset-0 flex items-center justify-center bg-gray-100 bg-opacity-75 rounded-md">
-                <span class="text-gray-700 font-semibold">Traitement en cours...</span>
+                <span class="font-semibold text-gray-700">Traitement en cours...</span>
             </div>
         </div>
 
