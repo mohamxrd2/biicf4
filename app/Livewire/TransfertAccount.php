b1228e46e2708f2f953dbf0922f5eda345e14d61
MacBook Pro <macbookpro@MacBook-Pro-de-Mohamed.local>
2024-11-06 08:48:16 +0000

    add

--- app/Livewire/TransfertAccount.php
+++ app/Livewire/TransfertAccount.php
@@ -2,17 +2,18 @@
 
 namespace App\Livewire;
 
-use App\Models\Cedd;
-use App\Models\Cefp;
 use App\Models\Cfa;
 use App\Models\Coi;
+use App\Models\Cedd;
+use App\Models\Cefp;
 
 use App\Models\User;
 use App\Models\Wallet;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-
 use Livewire\Component;
+use App\Models\Transaction;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 class TransfertAccount extends Component
 {
@@ -37,7 +38,6 @@ class TransfertAccount extends Component
         Log::info('User Wallet:', ['wallet' => $this->userWallet]);
 
         $this->coi = Coi::where('id_wallet', $this->userWallet->id)->first();
-        $this->cfa = Cfa::where('id_wallet', $this->userWallet->id)->first();
         $this->cedd = Cedd::where('id_wallet', $this->userWallet->id)->first();
         $this->cefd = Cefp::where('id_wallet', $this->userWallet->id)->first();
     }
@@ -68,11 +68,12 @@ class TransfertAccount extends Component
 
     try {
         // Démarrer une transaction
-       
+        $referenceId = $this->generateIntegerReference();
 
         // Débiter le compte de départ
         if ($this->account1 === 'COC') {
             $accountFrom->balance -= $this->amount;
+
         } else {
             $accountFrom->Solde -= $this->amount;
         }
@@ -85,6 +86,10 @@ class TransfertAccount extends Component
             $accountTo->Solde += $this->amount;
         }
         $accountTo->save();
+
+        
+        $this->createTransactionNew(Auth::id(), Auth::id(), 'Réception', $this->account2 , $this->amount, $referenceId, 'Transfert entre sous compte');
+        $this->createTransactionNew(Auth::id(), Auth::id(), 'Envoie', $this->account1, $this->amount, $referenceId, 'Transfert entre sous compte');
 
 
         // Message de succès
@@ -110,8 +115,7 @@ class TransfertAccount extends Component
                 return $this->userWallet;
             case 'COI':
                 return $this->coi;
-            case 'CFA':
-                return $this->cfa;
+          
             case 'CEDD':
                 return $this->cedd;
             case 'CEFP':
@@ -119,6 +123,29 @@ class TransfertAccount extends Component
             default:
                 return null;
         }
+    }
+
+    protected function createTransactionNew(int $senderId, int $receiverId, string $type, string $type_compte, float $amount, int $reference_id, string $description)
+    {
+
+        $transaction = new Transaction();
+        $transaction->sender_user_id = $senderId;
+        $transaction->receiver_user_id = $receiverId;
+        $transaction->type = $type;
+        $transaction->type_compte = $type_compte;
+        $transaction->amount = $amount;
+        $transaction->reference_id = $reference_id;
+        $transaction->description = $description;
+        $transaction->status = 'effectué';
+        $transaction->save();
+    }
+    protected function generateIntegerReference(): int
+    {
+        // Récupère l'horodatage en millisecondes
+        $timestamp = now()->getTimestamp() * 1000 + now()->micro;
+
+        // Retourne l'horodatage comme entier
+        return (int) $timestamp;
     }
 
     public function render()
