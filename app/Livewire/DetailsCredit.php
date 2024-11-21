e95fb882c2adf043368284c662cf734c61b0d8be
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-11-21 05:58:39 +0000

    chance code fusion modif

--- app/Livewire/DetailsCredit.php
+++ app/Livewire/DetailsCredit.php
@@ -100,7 +100,7 @@ class DetailsCredit extends Component
             return;
         }
         // Date du plus ancien commentaire
-        $this->timeFin = $this->demandeCredit->date_fin;
+        $this->timeFin = Carbon::parse($this->demandeCredit->date_fin);
 
         // Vérifier si le numéro de téléphone de l'utilisateur existe dans la table user_promir
         $this->userInPromir = UserPromir::where('numero', $userNumber)->first();
