bb85a445dc10717f7ba1c5d39b4ceecc9033ca83
ahondjo kwonjin <k.ahondjo98@gmail.com>
2025-01-04 03:02:30 +0000

    etape I achatD

--- resources/views/livewire/notification-detail.blade.php
+++ resources/views/livewire/notification-detail.blade.php
@@ -1,67 +1,67 @@
 <div>
     @if ($notification)
-        @switch($notification->type)
-            {{-- Achat Direct --}}
-            @case ('App\Notifications\AchatBiicf')
-                @livewire('Achatdirect', ['id' => $notificationId])
-            @case ('App\Notifications\livraisonAchatdirect')
-                @livewire('livraisonAchatdirect', ['id' => $notificationId])
-            @case ('App\Notifications\CountdownNotificationAd')
-                @livewire('CountdownNotificationAd', ['id' => $notificationId])
-            @case ('App\Notifications\commandVerifAd')
-                @livewire('command-verif-ad', ['id' => $notificationId])
-            @case ('App\Notifications\mainleveAd')
-                @livewire('mainleve-ad', ['id' => $notificationId])
-            @case ('App\Notifications\Confirmation')
-                @livewire('confirmation-notif', ['id' => $notificationId])
-                {{-- Appel Offre Direct --}}
-            @case ('App\Notifications\AppelOffre')
-                @livewire('appeloffre', ['id' => $notificationId])
-            @case ('App\Notifications\AppelOffreTerminer')
-                @livewire('appeloffreterminer', ['id' => $notificationId])
-                {{-- Appel offre grouper --}}
-            @case ('App\Notifications\AOGrouper')
-                @livewire('appeloffregrouper', ['id' => $notificationId])
-            @case ('App\Notifications\AppelOffreGrouperNotification')
-                @livewire('appeloffregroupernegociation', ['id' => $notificationId])
-            @case ('App\Notifications\AppelOffreTerminerGrouper')
-                @livewire('appeloffreterminergrouper', ['id' => $notificationId])
-                {{-- fournisseur offre negocier --}}
-            @case ('App\Notifications\OffreNotifGroup')
-                @livewire('enchere', ['id' => $notificationId])
-            @case ('App\Notifications\NegosTerminer')
-                @livewire('offrenegosterminer', ['id' => $notificationId])
-                {{-- fournisseur offre grouper --}}
-            @case ('App\Notifications\OffreNegosNotif')
-                @livewire('offre-groupe-quantite', ['id' => $notificationId])
-            @case ('App\Notifications\OffreNegosDone')
-                @livewire('Offre-negos-done', ['id' => $notificationId])
-                {{--  retrait  --}}
-            @case ('App\Notifications\Retrait')
-                @livewire('retrait', ['id' => $notificationId])
-            @case ('App\Notifications\DepositSos')
-                @livewire('deposit-sos', ['id' => $notificationId])
-            @case ('App\Notifications\DepositRecu')
-                @livewire('deposit-recu', ['id' => $notificationId])
-            @case ('App\Notifications\DepositSend')
-                @livewire('deposit-send', ['id' => $notificationId])
-            @case ('App\Notifications\RetraitCode')
-                @livewire('retrait-code', ['id' => $notificationId])
-                {{-- general --}}
-            @case ('App\Notifications\VerifUser')
-                @livewire('verif-user', ['id' => $notificationId])
-            @case ('App\Notifications\mainleveclient')
-                @livewire('mainleveclient', ['id' => $notificationId])
-                {{-- Ajoutez les autres cas selon vos besoins --}}
+        {{-- Achat Direct --}}
+        @if ($notification->type === 'App\Notifications\AchatBiicf')
+            @livewire('Achatdirect', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\livraisonAchatdirect')
+            @livewire('livraisonAchatdirect', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\CountdownNotificationAd')
+            @livewire('CountdownNotificationAd', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\commandVerifAd')
+            @livewire('command-verif-ad', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\mainleveAd')
+            @livewire('mainleve-ad', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\Confirmation')
+            @livewire('confirmation-notif', ['id' => $notificationId])
 
-                @default
-                    <div class="bg-white shadow rounded-lg p-6">
-                        <h2 class="text-xl font-semibold mb-4">{{ $notification->data['title'] ?? 'Notification' }}</h2>
-                        <p class="text-gray-600">{{ $notification->data['description'] ?? 'Aucun détail disponible' }}</p>
-                        <div class="mt-4 text-sm text-gray-500">
-                            {{ $notification->created_at->diffForHumans() }}
-                        </div>
-                    </div>
-            @endswitch
+
+            {{-- Appel Offre Direct --}}
+        @elseif ($notification->type === 'App\Notifications\AppelOffre')
+            @livewire('appeloffre', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\AppelOffreTerminer')
+            @livewire('appeloffreterminer', ['id' => $notificationId])
+
+            {{-- Appel offre grouper --}}
+        @elseif ($notification->type === 'App\Notifications\AOGrouper')
+            @livewire('appeloffregrouper', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\AppelOffreGrouperNotification')
+            @livewire('appeloffregroupernegociation', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\AppelOffreTerminerGrouper')
+            @livewire('appeloffreterminergrouper', ['id' => $notificationId])
+
+
+            {{-- fournisseur offre negocier --}}
+        @elseif ($notification->type === 'App\Notifications\OffreNotifGroup')
+            @livewire('enchere', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\NegosTerminer')
+            @livewire('offrenegosterminer', ['id' => $notificationId])
+
+
+            {{-- fournisseur offre grouper --}}
+        @elseif ($notification->type === 'App\Notifications\OffreNegosNotif')
+            @livewire('offre-groupe-quantite', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\OffreNegosDone')
+            @livewire('Offre-negos-done', ['id' => $notificationId])
+
+
+            {{--  retrait  --}}
+        @elseif ($notification->type === 'App\Notifications\Retrait')
+            @livewire('retrait', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\DepositSos')
+            @livewire('deposit-sos', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\DepositRecu')
+            @livewire('deposit-recu', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\DepositSend')
+            @livewire('deposit-send', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\RetraitCode')
+            @livewire('retrait-code', ['id' => $notificationId])
+
+
+            {{-- general --}}
+        @elseif ($notification->type === 'App\Notifications\VerifUser')
+            @livewire('verif-user', ['id' => $notificationId])
+        @elseif ($notification->type === 'App\Notifications\mainleveclient')
+            @livewire('mainleveclient', ['id' => $notificationId])
         @endif
-    </div>
+    @endif
+</div>
