a90426edc8db0694788f21d6dce63f530465789d
ahondjo kwonjin <k.ahondjo98@gmail.com>
2025-04-02 10:55:03 +0000

    tontine 1.3

--- app/Models/gelement.php
+++ app/Models/gelement.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class gelement extends Model
+class Gelement extends Model
 {
     use HasFactory;
 
